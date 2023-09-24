b9799c9f8289af0309a83bb7d2de3c93f888c8e8
Sean <seanfitzgerald05@gmail.com>
2023-09-24 20:58:16 +0100

    Made some updates to reflect the progress of the processing in the frontend

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,40 +1,21 @@
 import React, { useState } from 'react';
 import InputSection from './InputSection';
 import EmptySlots from './EmptySlots';
+import NumberGrid from './NumberGrid';
+import fillNewArray from '../utils/utils';
 
-
-const NumberGrid = ({ maxNumber, onSelect, onUnselect, selectedNumbers, highlightedNumbers }) => {
-  const setSelectedFieldColor = (num) => {
-    return selectedNumbers.includes(num) ? (maxNumber > 12 ? 'selected' : 'selected-lucky') : '';
-  }
-  const highlightedNumber = (num) => highlightedNumbers.includes(num) ? 'highlighted' : '';
-
-  let testId = maxNumber > 12 ? `normal-num` : 'bonus-num';
-
-  return (
-    <div className="number-grid">
-      {Array.from({ length: maxNumber }, (_, i) => i + 1).map((num) => (
-        <button
-          key={num}
-          className={`${highlightedNumber(num)} ${setSelectedFieldColor(num)}`}
-          data-testid={testId + num}
-          onClick={() => selectedNumbers.includes(num) ? onUnselect(selectedNumbers.indexOf(num)) : onSelect(num)}
-        >
-          {num}
-        </button>
-      ))}
-    </div>
-  );
-}
-
+fillNewArray
 const HomePage = () => {
-  const [selectedNumbers, setSelectedNumbers] = useState(Array(5).fill(null));
-  const [luckyStars, setLuckyStars] = useState(Array(2).fill(null));
-  const [highlightedNumbers, setHighlightedNumber] = useState(Array(5).fill(null));
-  const [highlightedLuckyStar, setHighlightedLuckyStar] = useState(Array(2).fill(null));
+  const [selectedNumbers, setSelectedNumbers] = useState(fillNewArray(5, null));
+  const [luckyStars, setLuckyStars] = useState(fillNewArray(2, null));
+  const [highlightedNumbers, setHighlightedNumber] = useState(fillNewArray(5, null));
+  const [highlightedLuckyStar, setHighlightedLuckyStar] = useState(fillNewArray(2, null));
   const [inputValue, setInputValue] = useState('');
+  const [sqlString, setSqlString] = useState('');
   const [queryResult, setQueryResult] = useState({});
+  const [statusOfprocessing, setStatusOfProcessing] = useState([]);
 
+  // Select a number from the Grid
   const selectNumber = (num) => {
     const firstEmptyIndex = selectedNumbers.indexOf(null);
     if (firstEmptyIndex !== -1) {
@@ -44,6 +25,7 @@ const HomePage = () => {
     }
   };
 
+  // Select a lucky star from the grid
   const selectLuckyStar = (num) => {
     const firstEmptyIndex = luckyStars.indexOf(null);
     if (firstEmptyIndex !== -1) {
@@ -54,36 +36,71 @@ const HomePage = () => {
     }
   };
 
+  // Deselect a number from the Grid
   const unselectNumber = (index) => {
     const newSelectedNumbers = [...selectedNumbers];
     newSelectedNumbers[index] = null;
     setSelectedNumbers(newSelectedNumbers.sort((a, b) => a - b));
   };
 
+  // Deselect one of the selected lucky stars
   const unselectLuckyStar = (index) => {
     const newLuckyStars = [...luckyStars];
     newLuckyStars[index] = null;
     setLuckyStars(newLuckyStars.sort((a, b) => a - b));
   };
 
+
   const checkForEnter = async (event) => {
     if (event.key === "Enter") {
-      setHighlightedNumber([null, null, null, null, null]);
-      setLuckyStars([null, null, null, null, null]);
+      await processPrompt();
+    }
+  }
 
+  // Main function to tranlaste user prompt into usable data
+  const processPrompt = async () => {
+    if (event.key === "Enter") {
+      setHighlightedNumber([null, null, null, null, null]);
+      setLuckyStars([null, null]);
+      let status = [];
+
+      status.push('Processing prompt');
+      status.push('Generating SQL from prompt');
+      setStatusOfProcessing(status);
+      console.log(status);
       const resultObj = await getSQLFromPrompt(inputValue);
+      status[status.length - 1] = 'SQL Generated';
+      status.push('Executing SQL');
+      setStatusOfProcessing(status);
+      console.log(status);
       const res = await runQueryOnDB(resultObj.sqlQuery);
+      status[status.length - 1] = 'SQL Executed';
+      status.push('Attempting to format results of SQL query');
+      setStatusOfProcessing(status);
+      console.log(status);
 
       let formatted = await formatFinalResult(resultObj, res);
-      highlightNumbers(formatted.suggestedNumbers)
+      status[status.length - 1] = 'Results have been reformatted';
+      status.push('Highlighting number');
+      setStatusOfProcessing(status);
+      console.log(status);
+      highlightNumbers(formatted.suggestedNumbers);
+
+      status[status.length - 1] = 'Numbers have been highlighted';
+      status.push('Done');
+      setStatusOfProcessing(status);
+      console.log(status);
+
     }
   };
 
+  // Set the highlighted numbers
   const highlightNumbers = (suggestedNumbers) => {
     setHighlightedNumber(suggestedNumbers['normalNumbers']);
     setHighlightedLuckyStar(suggestedNumbers['bonusNumbers']);
   }
 
+  // 
   const formatFinalResult = async (orinalQueries, sqlResult) => {
     const response = await fetch('/api/reformatResults', {
       method: 'POST',
@@ -95,7 +112,14 @@ const HomePage = () => {
     return data;
   };
 
+
   const runQueryOnDB = async (sqlQuery) => {
+
+    const disallowedSQLTerms = ["INSERT", "DELETE", "UPDATE", "ALTER", "DROP", "CREATE"];
+    if (disallowedSQLTerms.some(term => sqlQuery.toLowerCase().includes(term.toLowerCase()))) {
+      throw new Error(`ERROR: The provided SQL query is not allowed`);
+    }
+
     const response = await fetch("/api/queryDB", {
       method: 'POST',
       body: JSON.stringify({ sqlQuery }),
@@ -131,7 +155,7 @@ const HomePage = () => {
   return (
     <div className="App">
       <div className="main-container">
-        <InputSection inputValue={inputValue} onKeyDown={checkForEnter} onChange={updateInput} />
+        <InputSection inputValue={inputValue} onKeyDown={checkForEnter} onChange={updateInput} status={statusOfprocessing} />
         <div className="numbers-section">
           <div className="empty-slots-container">
             <div data-testid="normal-numbers-selected">
